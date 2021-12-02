a68b49291de13636ec428c1a8c4eca419e5dfd86
Robin Gauthey <gautheyrobin@gmail.com>
2021-12-02 16:38:44 +0100

    Avancement formulaire

--- ProjetM1.py
+++ ProjetM1.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 # Powershell = $env:FLASK_APP = "ProjetM1"
 # cmd = set FLASK_APP=ProjetM1
 # flask run
