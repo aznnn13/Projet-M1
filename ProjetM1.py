d4c32e6a16f85a0dad23d6afaf5641b93c3e7f3b
Robin Gauthey <gautheyrobin@gmail.com>
2021-12-18 01:38:31 +0100

    fix bug

--- ProjetM1.py
+++ ProjetM1.py
@@ -24,51 +24,52 @@ app.config['UPLOAD_FOLDER'] = 'static/csv'
 app.secret_key = 'super secret key'
 
 
-
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/result/', methods=['GET', 'POST'])
 def result():
     if request.method == 'POST':
-        #Convertion de 'ImmutableMultiDict' en 'dict'
+        # Convertion de 'ImmutableMultiDict' en 'dict'
         data = request.form.to_dict()
 
-        #On récupère l'option choisie et on l'enlève au dict
+        # On récupère l'option choisie et on l'enlève au dict
         Option = int(data.pop('Option', None))
-
 
         if Option <= 2:
 
-            #Suppression des anciens résultats
+            # Suppression des anciens résultats
             if os.path.exists("static/csv/csvPoints.csv"):
                 os.remove("static/csv/csvPoints.csv")
             if os.path.exists("static/images/linear_regression.png"):
                 os.remove("static/images/linear_regression.png")
 
-            #Déclaration des tableaux X et Y pour la régression linéaire
+            # Déclaration des tableaux X et Y pour la régression linéaire
             x = []
             y = []
             maximumX = 0
 
-            if Option == 1: # Import .csv
+            if Option == 1:  # Import .csv
 
-                #Sauvegarde du csv avec un nom statique + vérification de l'extension
+                # Sauvegarde du csv avec un nom statique + vérification de l'extension
                 f = request.files['csvPoints']
                 if f and allowed_file(f.filename):
-                    f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename('csvPoints.csv'))) #secure_filename(f.filename)
+                    f.save(os.path.join(app.config['UPLOAD_FOLDER'],
+                                        secure_filename('csvPoints.csv')))  # secure_filename(f.filename)
                 else:
                     flash('Erreur: Choisir un fichier .csv', 'danger')
                     return redirect(url_for('index'))
 
                 # Read the csv file
-                df = pd.read_csv("static/csv/csvPoints.csv", sep=',')[['X','Y']]
-                for (colname,colval) in df.iteritems():
+                df = pd.read_csv("static/csv/csvPoints.csv", sep=',')[['X', 'Y']]
+                for (colname, colval) in df.iteritems():
                     if colname == 'X':
                         for value in colval.values:
                             x.append([value])
@@ -79,11 +80,11 @@ def result():
                         for value in colval.values:
                             y.append([value])
 
-            if Option == 2: # Ajout des points à la main
+            if Option == 2:  # Ajout des points à la main
 
                 # check if the post request has the file part
                 i = 0
-                for key,value in data.items():
+                for key, value in data.items():
                     if i % 2 == 0:
                         x.append([value])
                         value = float(value)
@@ -91,11 +92,11 @@ def result():
                             maximumX = value
                     else:
                         y.append([value])
-                    i+= 1
+                    i += 1
 
-            #Construction des listes X et Y
-            x = np.array(x,dtype=float)
-            y = np.array(y,dtype=float)
+            # Construction des listes X et Y
+            x = np.array(x, dtype=float)
+            y = np.array(y, dtype=float)
 
             # create a linear regression model
             model = LinearRegression()
@@ -117,13 +118,13 @@ def result():
             ax.axis('tight')
             plt.savefig('static/images/linear_regression.png')
 
-        else: #Nouveau formulaire
+        else:  # Nouveau formulaire
 
-            #Convertion des valeurs du dict en float
+            # Convertion des valeurs du dict en float
             data = dict((k, float(v)) for k, v in data.items())
 
             maxValue = max(data.values())
-            return render_template("result.html", Option=Option,maxValue=maxValue)
+            return render_template("result.html", Option=Option, maxValue=maxValue)
 
         return render_template("result.html", Option=Option)
 
